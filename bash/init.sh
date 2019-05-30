17fabe8b3bf4b5c8e4272158ad035f17ffb7b893
cfregly <chris@fregly.com>
2019-05-29 18:12:45 -0700

    removed kube

--- bash/init.sh
+++ bash/init.sh
@@ -5,7 +5,7 @@ apt-get install -y python3-pip
 apt-get install -y git
 
 pip3 install --upgrade pip
-pip install kubernetes
+#pip install kubernetes
 pip install cli-pipeline
 
 git clone https://github.com/PipelineAI
