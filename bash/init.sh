9f3ccb99ce87cd2ef6f60808470bca14b6942740
echiu3 <35546828+echiu3@users.noreply.github.com>
2019-05-31 15:08:40 -0500

    changed cli-pipeline flag

--- bash/init.sh
+++ bash/init.sh
@@ -1,13 +1,13 @@
 mkdir test1
-sudo apt-get update
+apt-get update
 mkdir test2
-sudo apt-get install -y python3-pip
+apt-get install -y python3-pip
 mkdir test3
 apt-get install -y git
 mkdir test4
 pip3 install --upgrade pip
 mkdir test5
-pip install cli-pipeline==1.5.315 --default-timeout=120 --ignore-installed --no-cache --upgrade
+pip install cli-pipeline==1.5.319 --default-timeout=120 --ignore-installed --no-cache --upgrade
 mkdir test6
 git clone https://github.com/PipelineAI
 mkdir test7
