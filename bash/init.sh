b5d4853dd276c71edda89336b9d4bec0e45dcb0d
echiu3 <35546828+echiu3@users.noreply.github.com>
2019-05-30 21:16:04 -0500

    Update cli-pipeline flags

--- bash/init.sh
+++ bash/init.sh
@@ -6,6 +6,6 @@ apt-get install -y git
 
 pip3 install --upgrade pip
 #pip install kubernetes
-pip install cli-pipeline
+pip install cli-pipeline==1.5.315 --default-timeout=120 --ignore-installed --no-cache --upgrade
 
 git clone https://github.com/PipelineAI
