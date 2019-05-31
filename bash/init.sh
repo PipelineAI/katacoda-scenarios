7819d51a6196939a32a6b4cfe145f454e08d4442
echiu3 <35546828+echiu3@users.noreply.github.com>
2019-05-30 22:11:55 -0500

    added mkdir tests

--- bash/init.sh
+++ bash/init.sh
@@ -1,7 +1,13 @@
-mkdir test
+mkdir test1
 apt-get update
+mkdir test2
 apt-get install -y python3-pip
+mkdir test3
 apt-get install -y git
+mkdir test4
 pip3 install --upgrade pip
+mkdir test5
 pip install cli-pipeline==1.5.315 --default-timeout=120 --ignore-installed --no-cache --upgrade
+mkdir test6
 git clone https://github.com/PipelineAI
+mkdir test7
